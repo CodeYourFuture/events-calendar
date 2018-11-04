244300c69607fe871bef195c3c33149fef228a9f
Steve <mhdghanembalhawan@hotmail.com>
2018-11-04 21:37:40 +0000

    add navigation bar

--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -1,15 +1,20 @@
 import React from "react";
+import Bootstrap from "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./Navigation";
 const mainPage = () => (
-    <div className="container mt-5">
-        <h1 className="mb-3">CYF Events Calendar</h1>
-        <ul className="nav flex-column">
-            <li className="nav-item">
-                <a href="/admin">Admin</a>
-            </li>
-            <li className="nav-item">
-                <a href="/events">Events</a>
-            </li>
-        </ul>
+    <div>
+        <Navigation />
+        <div className="container mt-5">
+            <h1 className="mb-3">CYF Events Calendar</h1>
+            <ul className="nav flex-column">
+                <li className="nav-item">
+                    <a href="/admin">Admin</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/events">Events</a>
+                </li>
+            </ul>
+        </div>
     </div>
 );
 
